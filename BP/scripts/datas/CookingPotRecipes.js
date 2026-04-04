a2c90c59c7d0df22824f759f9036597e059d6061
尘鸢 <141193406+DustKite@users.noreply.github.com>
2026-04-04 22:59:30 +0800

    feat: add items, recipes, scripts, and textures
    Introduce a large set of new food items and related assets: numerous BP/items (consumables, block-placers, doughs), BP/recipes for crafting and furnace conversions, and BP/scripts (CookingPotRecipes, CuttingRecipes, EventAPI, ItemAPI, RandomAPI, registration scripts) to register foods, loot and recipes. Also add resource pack assets under RP/textures for items, blocks, and terrain/item texture mappings. These additions add new consumables, bowls/bottles conversions, and support scripts to enable their behavior in-game.

---
+++ BP/scripts/datas/CookingPotRecipes.js
@@ -0,0 +1,569 @@
+const CookingPotRecipes = [
+    {
+        "identifer": "farmersdelightplus:assorted_pufferfish_in_tomato_sauce",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 300,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "farmersdelightplus:pufferfish_slice"
+            },
+            {
+                "item": "minecraft:potato"
+            },
+            {
+                "item": "minecraft:brown_mushroom"
+            },
+            {
+                "tag": "farmersdelight:is_rice"
+            },
+            {
+                "tag": "farmersdelight:is_onion"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:assorted_pufferfish_in_tomato_sauce",
+            "count": 1
+        },
+        "container": {
+            "item": "farmersdelight:tomato_sauce"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:berry_compote",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:apple"
+            },
+            {
+                "tag": "forge:compote_berries"
+            },
+            {
+                "tag": "forge:compote_berries"
+            },
+            {
+                "item": "minecraft:sugar"
+            },
+            {
+                "item": "minecraft:sugar"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:berry_compote",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:glass_bottle"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:breaded_rice_balls",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:bread"
+            },
+            {
+                "item": "farmersdelight:rice"
+            },
+            {
+                "tag": "forge:risotto_meat"
+            },
+            {
+                "item": "minecraft:egg"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:breaded_rice_balls",
+            "count": 2
+        },
+        "container": {
+            "item": "minecraft:air"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:candied_slime",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 100,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:slime_ball"
+            },
+            {
+                "item": "minecraft:sugar"
+            },
+            {
+                "item": "minecraft:sugar"
+            },
+            {
+                "item": "minecraft:sweet_berries"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:candied_slime",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:air"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:chocolate_glazed_chicken",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:chicken"
+            },
+            {
+                "item": "minecraft:potato"
+            },
+            {
+                "tag": "farmersdelight:is_onion"
+            },
+            {
+                "item": "minecraft:cocoa_beans"
+            },
+            {
+                "item": "minecraft:cocoa_beans"
+            },
+            {
+                "item": "minecraft:sugar"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:chocolate_glazed_chicken",
+            "count": 1
+        },
+        "container": {
+            "item": "farmersdelight:cooked_rice"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:festive_porkchop_with_berries",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:porkchop"
+            },
+            {
+                "item": "minecraft:sweet_berries"
+            },
+            {
+                "item": "minecraft:sweet_berries"
+            },
+            {
+                "item": "minecraft:sugar"
+            },
+            {
+                "tag": "forge:salad_ingredients"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:festive_porkchop_with_berries",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:glow_ink_pasta",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            [
+                {
+                    "item": "minecraft:fish"
+                },
+                {
+                    "item": "minecraft:clownfish"
+                },
+                {
+                    "item": "minecraft:salmon"
+                },
+                {
+                    "tag": "farmersdelight:is_raw_fish"
+                }
+            ],
+            {
+                "tag": "forge:pasta/raw_pasta"
+            },
+            {
+                "item": "minecraft:glow_ink_sac"
+            },
+            {
+                "item": "minecraft:carrot"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:glow_ink_pasta",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:heart_of_the_minotaur_block",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 300,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:beef"
+            },
+            {
+                "item": "farmersdelight:mutton_chops"
+            },
+            {
+                "item": "farmersdelight:mutton_chops"
+            },
+            {
+                "item": "minecraft:carrot"
+            },
+            {
+                "item": "minecraft:beetroot"
+            },
+            {
+                "tag": "forge:salad_ingredients"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:heart_of_the_minotaur_block",
+            "count": 1
+        },
+        "container": {
+            "item": "farmersdelight:tomato_sauce"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:honeyed_rice_with_berries",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "tag": "farmersdelight:is_rice"
+            },
+            {
+                "item": "minecraft:sweet_berries"
+            },
+            {
+                "item": "minecraft:honey_bottle"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:honeyed_rice_with_berries",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:honeyed_rice_with_dragon_egg_block",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 400,
+        "experience": 2.0,
+        "ingredients": [
+            {
+                "item": "minecraft:dragon_egg"
+            },
+            {
+                "item": "minecraft:golden_apple"
+            },
+            {
+                "item": "minecraft:carrot"
+            },
+            {
+                "tag": "farmersdelight:is_onion"
+            },
+            {
+                "item": "minecraft:salmon"
+            },
+            {
+                "item": "minecraft:honey_bottle"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:honeyed_rice_with_dragon_egg_block",
+            "count": 1
+        },
+        "container": {
+            "item": "farmersdelight:cooked_rice"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:lagman",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "tag": "forge:pasta/raw_rice_noodle"
+            },
+            [
+                {
+                    "item": "minecraft:beef"
+                },
+                {
+                    "tag": "farmersdelight:is_raw_beef"
+                }
+            ],
+            {
+                "tag": "farmersdelight:is_onion"
+            },
+            {
+                "tag": "forge:lagman_base"
+            },
+            {
+                "tag": "forge:lagman_stuff"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:lagman",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:pho_soup",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            [
+                {
+                    "item": "minecraft:beef"
+                },
+                {
+                    "tag": "farmersdelight:is_raw_beef"
+                }
+            ],
+            {
+                "tag": "forge:pasta/raw_rice_noodle"
+            },
+            {
+                "tag": "farmersdelight:is_onion"
+            },
+            {
+                "item": "minecraft:seagrass"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:pho_soup",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:rice_pilaf",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "tag": "farmersdelight:is_rice"
+            },
+            {
+                "tag": "forge:raw_mutton"
+            },
+            {
+                "item": "minecraft:carrot"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:rice_pilaf",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:rice_with_frogspawn",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:kelp"
+            },
+            {
+                "item": "minecraft:tropical_fish"
+            },
+            {
+                "item": "minecraft:frogspawn"
+            },
+            {
+                "tag": "farmersdelight:is_rice"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:rice_with_frogspawn",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:risotto",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "tag": "farmersdelight:is_rice"
+            },
+            {
+                "tag": "forge:risotto_meat"
+            },
+            {
+                "item": "minecraft:sea_pickle"
+            },
+            {
+                "item": "minecraft:kelp"
+            },
+            {
+                "tag": "farmersdelight:is_onion"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:risotto",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:sparkling_potato",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 200,
+        "experience": 1.0,
+        "ingredients": [
+            {
+                "item": "minecraft:potato"
+            },
+            {
+                "item": "minecraft:glow_berries"
+            },
+            {
+                "tag": "forge:salad_ingredients"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:sparkling_potato",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:bowl"
+        }
+    },
+    {
+        "identifer": "farmersdelightplus:turtle_soup",
+        "tags": [
+            "cooking_pot"
+        ],
+        "priority": 0,
+        "time": 400,
+        "experience": 1.5,
+        "ingredients": [
+            {
+                "item": "minecraft:turtle_egg"
+            },
+            [
+                {
+                    "item": "minecraft:fish"
+                },
+                {
+                    "item": "minecraft:clownfish"
+                },
+                {
+                    "item": "minecraft:salmon"
+                },
+                {
+                    "tag": "farmersdelight:is_raw_fish"
+                }
+            ],
+            {
+                "item": "minecraft:kelp"
+            },
+            {
+                "item": "minecraft:carrot"
+            }
+        ],
+        "result": {
+            "item": "farmersdelightplus:turtle_soup",
+            "count": 1
+        },
+        "container": {
+            "item": "minecraft:scute"
+        }
+    }
+];
+export { CookingPotRecipes };
---
+++ BP/scripts/datas/CuttingRecipes.js
@@ -0,0 +1,91 @@
+export const CuttingBoardRecipes = [
+    {
+        "ingredients": {
+            "item": "minecraft:pumpkin"
+        },
+        "result": [
+            {
+                "item": "minecraft:carved_pumpkin",
+                "count": 1
+            },
+            {
+                "item": "minecraft:pumpkin_seeds",
+                "count": 4
+            }
+        ],
+        "tool": {
+            "tag": "minecraft:is_shears"
+        },
+        "is_block_type": true,
+        "sound": "use.wood"
+    },
+    {
+        "ingredients": {
+            "item": "minecraft:pufferfish"
+        },
+        "result": [
+            {
+                "item": "farmersdelightplus:pufferfish_slice",
+                "count": 2
+            },
+            {
+                "item": "minecraft:bone_meal",
+                "count": 1
+            }
+        ],
+        "tool": {
+            "tag": "farmersdelight:is_knife"
+        },
+        "is_block_type": false,
+        "sound": "use.wood"
+    },
+    {
+        "ingredients": {
+            "tag": "forge:rice_dough"
+        },
+        "result": [
+            {
+                "item": "farmersdelightplus:raw_rice_noodle",
+                "count": 1
+            }
+        ],
+        "tool": {
+            "tag": "farmersdelight:is_knife"
+        },
+        "is_block_type": false,
+        "sound": "use.wood"
+    },
+    {
+        "ingredients": {
+            "tag": "minecraft:wool"
+        },
+        "result": [
+            {
+                "item": "minecraft:string",
+                "count": 1,
+                "chance": 0.5
+            }
+        ],
+        "tool": {
+            "tag": "minecraft:is_shears"
+        },
+        "is_block_type": false,
+        "sound": "use.wood"
+    },
+    {
+        "ingredients": {
+            "item": "farmersdelight:wheat_dough"
+        },
+        "result": [
+            {
+                "item": "farmersdelight:raw_pasta",
+                "count": 1
+            }
+        ],
+        "tool": {
+            "tag": "farmersdelight:is_knife"
+        },
+        "is_block_type": false,
+        "sound": "use.wood"
+    }
+];
